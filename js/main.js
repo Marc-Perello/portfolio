86813d5331ec35e2a3ce0bc61562e16565fb9fd6
Marc Perelló <markperello@gmail.com>
2016-10-03 20:31:51 +0200

    Landing acabada LMS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 
+
+$(document).ready(function(){
+
+$("#featured-projects-link").click(function() {
+$('html, body').animate({
+   scrollTop: $("#featured-projects").offset().top -5
+  }, 1200);
+});
+
+});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#featured-projects-link").click(function(){$("html, body").animate({scrollTop:$("#featured-projects").offset().top-5},1200)})});
